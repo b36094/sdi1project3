2e5d531ca6d8cb719067f79a45de7d5c34da92c4
Nactus <nactusberrilli@gmail.com>
2013-01-23 19:11:56 -0800

    initial variables

--- Birsan_Andrei_Assignment3.js
+++ Birsan_Andrei_Assignment3.js
@@ -1,1 +1,28 @@
-alert("JavaScript works!");
+//Andrei Birsan
+//SDI1 Project 3
+//1-23-2013
+
+//Global Variables Declaration
+
+var	gameName = "Space Flight Simulator",
+		gameVersion = 2,
+		gameSubVersion = 3,		
+		gateNum = 3,
+		drivingStation = 2,
+		userInfoSave = { "userName": "Andrei", "userMiles": 55, "userLocation": "Milky-Way" };
+		
+
+//
+if (gameVersion >= 2) {
+	
+	if (gameSubVersion >= 3) {
+		
+		//log welcome message and concatinate version and subversion
+		console.log("Welcome to "+ gameName +" "+ gameVersion +"."+ gameSubVersion);	
+		console.log("You are up to date!");		
+	}
+	
+	else {
+		}	
+}	
+		

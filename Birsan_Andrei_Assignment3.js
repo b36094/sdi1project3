8615362d61a764fc4211c4c32abc39b10b3ca154
Nactus <nactusberrilli@gmail.com>
2013-01-23 19:55:00 -0800

    conditional & nested conditional added

--- Birsan_Andrei_Assignment3.js
+++ Birsan_Andrei_Assignment3.js
@@ -4,25 +4,50 @@
 
 //Global Variables Declaration
 
-var	gameName = "Space Flight Simulator",
-		gameVersion = 2,
-		gameSubVersion = 3,		
-		gateNum = 3,
+var	gameName = "Nagra Space Flight Simulator",
+		gameVersion = 2,															// ==> switch
+		gameSubVersion = 3,														// ==> switch
+		gateNum = 0,
 		drivingStation = 2,
-		userInfoSave = { "userName": "Andrei", "userMiles": 55, "userLocation": "Milky-Way" };
+		
+		userInfoSave = { "userName": "Andrei", 
+							  "userMiles": 0, 
+							  "userLocation": "Milky-Way",
+							  "shipName": "" 
+		};
 		
 
-//
+//Conditional (version - subversion check)
+
 if (gameVersion >= 2) {
 	
+	//Nested Conditional
 	if (gameSubVersion >= 3) {
 		
-		//log welcome message and concatinate version and subversion
+		//output welcome message and concatenate version and subversion
 		console.log("Welcome to "+ gameName +" "+ gameVersion +"."+ gameSubVersion);	
-		console.log("You are up to date!");		
+		console.log("Your Nagra client is up to date!");	
+		
+			while(gateNum < 3)	{
+				
+				console.log("Loading ... Please wait");
+				gateNum++;	
+				
+			}
+			console.log("The gate "+ gateNum +" is now available for you.");
+			console.log("Please proceed to your ship.");
 	}
 	
+	//ewer subversion available	
 	else {
+		
+		console.log("There is a newer subversion available on our inter-galactic site.")			
+		
 		}	
 }	
+
+else {
+	console.log("There is a newer version of the program available for download. Please update before playing.");	
+	
+}
 		
